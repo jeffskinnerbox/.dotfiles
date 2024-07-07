f0ba5d574c0420a25b3ebc23bfeddef0a1811c4b
jeffskinnerbox <jeff.irland@verizon.net>
2024-07-07 11:31:47 -0400

    minor change

--- pkg-nvim/nvim/lua/active/keymappings.lua
+++ pkg-nvim/nvim/lua/active/keymappings.lua
@@ -1,4 +1,3 @@
-
 --------------------------------------------------------------------------------
 -- active/keymappings.lua
 
@@ -21,17 +20,16 @@
 --    [Neovim for Beginners — Key Mappings and WhichKey](https://alpha2phi.medium.com/neovim-for-beginners-key-mappings-and-whichkey-31dbf58f9f87)
 --------------------------------------------------------------------------------
 
-
 -- function that wraps vim.api.nvim_set_keymap command into something easy to use
 -- keymap(<mode>, <key-to-bind>, <action-wanted>, <options>)
 -- example:  keymap('n', '<leader>sp', ':setlocal spell!<cr>', opts)  -- toggle on/off spell checking
 local keymap = function(mode, key, result, options)
-  vim.api.nvim_set_keymap(
-    mode,                 -- mode can be 'n' = normal mode, 'i' = insert mode, 'v' = visual mode, 'x' = visual block mode, 't' = term mode, 'c' = command mode
-    key,                  -- key sequence to trigger result
-    result,               -- command or key subsituation to be made
-    options               -- keymap options
-  )
+	vim.api.nvim_set_keymap(
+		mode, -- mode can be 'n' = normal mode, 'i' = insert mode, 'v' = visual mode, 'x' = visual block mode, 't' = term mode, 'c' = command mode
+		key, -- key sequence to trigger result
+		result, -- command or key subsituation to be made
+		options -- keymap options
+	)
 end
 
 -- potential values for 'options'
@@ -39,37 +37,36 @@ local opts = { noremap = true, silent = true }
 local expr_opts = { noremap = true, expr = true, silent = true }
 local term_opts = { silent = true }
 
-
 -- the <leader> key is set in 'settings.lua' file
 
 -- key mapping for <Space> to no-operation so its not the leader key
-keymap('n', '<Space>', '<Nop>', opts)                     -- my leader is set in file 'settings.lua'
+keymap("n", "<Space>", "<Nop>", opts) -- my leader is set in file 'settings.lua'
 
 -- key mapping for changing current working directory
-keymap('n', '<leader>cd', ':cd %:p:h<cr>:pwd<cr>', opts)  -- change working directory to same as file within buffer
+keymap("n", "<leader>cd", "<cmd>cd %:p:h<cr><cmd>pwd<cr>", opts) -- change working directory to same as file within buffer
 
 -- key mapping for line wrapping
-keymap('n', '<leader>wr', ':set wrap<cr>', opts)          -- turn-on line wrapping
-keymap('n', '<leader>nwr', ':set nowrap<cr>', opts)       -- turn-off line wrapping
+keymap("n", "<leader>wr", ":set wrap<cr>", opts) -- turn-on line wrapping
+keymap("n", "<leader>nwr", ":set nowrap<cr>", opts) -- turn-off line wrapping
 
 -- key mappings for search highlighting
-keymap('n', '<leader>n', ':nohlsearch<cr>', opts)        -- turn-off search term highlighting
+keymap("n", "<leader>n", ":nohlsearch<cr>", opts) -- turn-off search term highlighting
 
 -- key mappings for revealing hidden characters
-keymap('n', '<leader>hc', ':set list!<cr>', opts)        -- toggle between showing and hiding hidden characters
+keymap("n", "<leader>hc", ":set list!<cr>", opts) -- toggle between showing and hiding hidden characters
 vim.cmd([[set listchars=trail:·,precedes:«,extends:»,eol:↲,tab:▸\ ]]) -- enumerates which hidden characters to display and which character to display them with
 
 -- key mappings for spelling checker
-keymap('n', '<leader>sp', ':setlocal spell!<cr>', opts)  -- toggle on/off spell checking
-keymap('n', '<leader>sd', ']s', opts)                    --  move to the next misspelled word
-keymap('n', '<leader>su', '[s', opts)                    --  move to the previous misspelled word
-keymap('n', '<leader>sa', 'zg', opts)                    --  add a word to the dictionary
-keymap('n', '<leader>sua', 'zug', opts)                  --  undo the addition of a word to the dictionary
-keymap('n', '<leader>s?', 'z=', opts)                    --  view spelling suggestions for a misspelled word
+keymap("n", "<leader>sp", ":setlocal spell!<cr>", opts) -- toggle on/off spell checking
+keymap("n", "<leader>sd", "]s", opts) --  move to the next misspelled word
+keymap("n", "<leader>su", "[s", opts) --  move to the previous misspelled word
+keymap("n", "<leader>sa", "zg", opts) --  add a word to the dictionary
+keymap("n", "<leader>sua", "zug", opts) --  undo the addition of a word to the dictionary
+keymap("n", "<leader>s?", "z=", opts) --  view spelling suggestions for a misspelled word
 
 -- key mappings for moving between buffers
-keymap('n', '<TAB>', '<cmd>bn<cr>', opts)                -- move to next buffer
-keymap('n', '<S-TAB>', '<cmd>bp<cr>', opts)              -- move to previous buffer
+keymap("n", "<TAB>", "<cmd>bn<cr>", opts) -- move to next buffer
+keymap("n", "<S-TAB>", "<cmd>bp<cr>", opts) -- move to previous buffer
 
 -- key mapping for disabling arrow keys to force use of h, j, k, l keys for cursor navigation
 --keymap('n', '<up>', '<nop>', opts)
@@ -86,4 +83,3 @@ keymap('n', '<S-TAB>', '<cmd>bp<cr>', opts)              -- move to previous buf
 --keymap('n', '<Right>', ':vertical resize -1<CR>', opts)
 --keymap('n', '<Up>', ':resize -1<CR>', opts)
 --keymap('n', '<Down>', ':resize +1<CR>', opts)
-
