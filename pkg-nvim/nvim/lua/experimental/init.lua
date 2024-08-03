52af3697a4e48b8f449c42636dea5c72300cde32
jeffskinnerbox <jeff.irland@verizon.net>
2024-08-03 13:37:13 -0400

    working towards a fix for 'undo' directory

--- pkg-nvim/nvim/lua/experimental/init.lua
+++ pkg-nvim/nvim/lua/experimental/init.lua
@@ -1,4 +1,4 @@
--- vim: set ts=2 sw=2 sts=2 et ai:                                              -- modeline, equvalent to 'vim: set tabstop=2 shiftwidth=2 softtabstop=2 expandtab autoindent filetype=markdown:'
+-- vim: set ts=2 sw=2 sts=2 et ai:                                              -- modeline, equvalent to 'vim: set tabstop=2 shiftwidth=2 softtabstop=2 expandtab autoindent filetype=lua:'
 
 -- luacheck: globals vim
 -- luacheck: max line length 300
